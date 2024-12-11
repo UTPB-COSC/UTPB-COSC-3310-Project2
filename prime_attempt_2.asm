4069794e2857cc9e30b9c01d1b1a37e27e3f3705
melanycoss <mcoss2103@gmail.com>
2024-12-11 15:01:29 -0600

    replaced r10 with rcx, replaced previous instances of rcx with rbx, and created a qword to hold the value of rcx
    I was having the problem of the loop not continuing after the first iteration, found that the problem was happening after executing printing call so I held the value of rcx prior to printing in the loop_counter variable then reassigned it after printing to ensure it was incrementing.

--- prime_attempt_2.asm
+++ prime_attempt_2.asm
@@ -5,30 +5,33 @@ not_prime db ' is not prime', 0x0a, 0
 not_prime_len equ $ - not_prime
 buffer db 0, 0
 
+section.bss
+loop_counter dq 
+
 section .text
 global _start
 
 _start:
-    mov r10, 1           ; initialize loop counter to 1
+    mov rcx, 1           ; initialize loop counter to 1
 
 loop_head:
-    cmp r10, 100              ; checks to ensure r10 is less than 100
+    cmp rcx, 100              ; checks to ensure r10 is less than 100
     jge loop_exit            ; exits if r10 >= 100
 
 loop_body:
     ; load number into rax
-    mov rax, r10
+    mov rax, rcx
     ; ensures the first few prime numbers (necessary for rest of code to work) print
     ; compares current num in r10 to 1, 2, 3, 5, and 7 and sends them to print
-    cmp al, 1 
+    cmp rax, 1 
     je is_not_prime_10
-    cmp al, 2
+    cmp rax, 2
     je is_prime_10
-    cmp al, 3
+    cmp rax, 3
     je is_prime_10
-    cmp al, 5
+    cmp rax, 5
     je is_prime_10
-    cmp al, 7
+    cmp rax, 7
     je is_prime_10
     
     test rax, 1              ; checks least significant bit (if it's 0 then it is automatically not prime)
@@ -36,30 +39,30 @@ loop_body:
 
 ; checks if divisible by 3
     mov rdx, 0               ; clear rdx (holds remainder)
-    mov rcx, 3               ; divisor (3)
-    div rcx                  ; performs division: rax = rax / 3, rdx = remainder
+    mov rbx, 3               ; divisor (3)
+    div rbx                  ; performs division: rax = rax / 3, rdx = remainder
     cmp rdx, 0               ; checks if remainder is zero
     je is_not_prime_10          ; if divisible by 3, it's not prime
 
     ; checks if divisible by 5
-    mov rax, r10 ; makes sure rax is r10
+    mov rax, rcx ; makes sure rax is r10
     mov rdx, 0               ; clear rdx (holds remainder)
-    mov rcx, 5               ; divisor (5)
-    div rcx                  ; performs division: rax = rax / 5, rdx = remainder
+    mov rbx, 5               ; divisor (5)
+    div rbx                  ; performs division: rax = rax / 5, rdx = remainder
     cmp rdx, 0               ; checks if remainder is zero
     je is_not_prime_10          ; if divisible by 5, it's not prime
 
     ; checks if divisible by 7
-    mov rax, r10 ; makes sure rax is r10
+    mov rax, rcx ; makes sure rax is r10
     mov rdx, 0               ; clear rdx (holds remainder)
-    mov rcx, 7               ; divisor (7)
-    div rcx                  ; performs division: rax = rax / 5, rdx = remainder
+    mov rbx, 7               ; divisor (7)
+    div rbx                  ; performs division: rax = rax / 5, rdx = remainder
     cmp rdx, 0               ; checks if remainder is zero
     je is_not_prime_10          ; if divisible by 7, it's not prime
     
     
   is_prime_10:
-    mov rax, r10      ; makes sure rax is r10
+    mov rax, rcx      ; makes sure rax is r10
 
     ; the following addresses digits with ones and tens places by dividing it by 10
     ; to have two separate ASCII values printed consecutively in the buffer register
@@ -77,6 +80,8 @@ loop_body:
     mov al, dl       ; moves the ones digit to AL
     add al, '0'      ; convert the ones digit to ASCII
     mov [buffer+1], al ; stores the ASCII character in buffer[1]
+    
+    mov [loop_counter], rcx
 
     ; prints the number
     mov rax, 4       
@@ -91,12 +96,14 @@ loop_body:
     lea rcx, prime     
     mov rdx, prime_len   
     int 0x80
+    
+    mov rcx, [loop_counter]
     jmp increment
 
 is_not_prime_10:
     ; exactly the same as is_prime_10 
-    mov rax, r10      
-
+    mov rax, rcx      
+    
     mov rbx, 10      
     xor rdx, rdx     
     div rbx      
@@ -107,6 +114,8 @@ is_not_prime_10:
     mov al, dl       
     add al, '0'      
     mov [buffer+1], al 
+    
+    mov [loop_counter], rcx
 
     mov rax, 4       
     mov rbx, 1       
@@ -116,13 +125,15 @@ is_not_prime_10:
 
     mov rbx, 1              
     mov rax, 4              
-    lea rcx, not_prime     
+    mov rcx, not_prime     
     mov rdx, not_prime_len   
     int 0x80
+    
+    mov rcx, [loop_counter]
     jmp increment ; goes to increment
     
 increment:
-    inc r10       ; increases r10 by 1           
+    inc rcx       ; increases r10 by 1           
     jmp loop_head    ; goes to the loop head        
 
 loop_exit:
